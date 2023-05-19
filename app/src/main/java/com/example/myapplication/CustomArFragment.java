721f67137888526fa536e33d3be18c2ea4eead5a
Alter <crimson.lotus.201@gmail.com>
2023-05-19 09:36:20 +0700

    Update Async + Firebase

--- app/src/main/java/com/example/myapplication/CustomArFragment.java
+++ app/src/main/java/com/example/myapplication/CustomArFragment.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +17,19 @@ import com.google.ar.core.AugmentedImageDatabase;
 import com.google.ar.core.Config;
 import com.google.ar.core.Session;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomArFragment extends ArFragment {
-
+    List<Bitmap> images = new ArrayList<>();
+    List<String> names = new ArrayList<>();
     @Override
     protected Config getSessionConfiguration(Session session) {
 
@@ -25,6 +38,10 @@ public class CustomArFragment extends ArFragment {
 
         AugmentedImageDatabase aid = new AugmentedImageDatabase(session);
 
+        AsyncGettingBitmapFromUrl asyncGettingBitmapFromUrl = new AsyncGettingBitmapFromUrl();
+//        asyncGettingBitmapFromUrl.execute();
+//        for (int i = 0; i < images.size(); i++) aid.addImage(names.get(i), images.get(i));
+        
         Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.image);
         aid.addImage("image", image);
 
@@ -44,5 +61,49 @@ public class CustomArFragment extends ArFragment {
         getPlaneDiscoveryController().setInstructionView(null);
 
         return frameLayout;
+    }
+
+    @Nullable
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            return BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            // Log exception
+            return null;
+        }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class AsyncGettingBitmapFromUrl extends AsyncTask<String, Void, Bitmap> {
+        final String TAG = "Read Data Activity";
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            FirebaseFirestore db;
+
+            db = FirebaseFirestore.getInstance();
+            db.collection("map")
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                String imageUrl = document.getData().get("image").toString();
+                                String name = document.getData().get("name").toString();
+                                Bitmap image = getBitmapFromURL(imageUrl);
+                                images.add(image);
+                                names.add(name);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    });
+            return null;
+        }
     }
 }
